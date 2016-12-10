820b2c46e945de9e8fbd787564bee2b742b7d7f5
Tuomas Haapala <tuomas.haapala@anders.fi>
2016-12-10 12:23:39 +0200

    Fix overlapping urlpatterns

--- parkkihubi/urls.py
+++ parkkihubi/urls.py
@@ -5,7 +5,7 @@ from parkings.api.internal import urls as internal_urls
 from parkings.api.operator import urls as operator_urls
 
 urlpatterns = [
-    url(r'^internal/v1/', include(internal_urls)),
-    url(r'^operator/v1/', include(operator_urls)),
+    url(r'^internal/v1/', include(internal_urls, namespace='internal')),
+    url(r'^operator/v1/', include(operator_urls, namespace='operator')),
     url(r'^admin/', admin.site.urls),
 ]
