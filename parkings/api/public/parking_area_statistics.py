58fe17a09080b12fcc126863e9df1b93fc0433c2
Max <maxgrasbeck@gmail.com>
2017-04-27 13:44:31 +0300

    Add parking area statistics real-time public API
    Adds a public API endpoint for viewing the amount of parkings currently
    ongoing for parking areas.

---
+++ parkings/api/public/parking_area_statistics.py
@@ -0,0 +1,38 @@
+from django.utils import timezone
+from rest_framework import serializers, viewsets
+
+from parkings.models import Parking, ParkingArea
+
+
+class ParkingAreaStatisticsSerializer(serializers.ModelSerializer):
+    current_parking_count = serializers.SerializerMethodField()
+
+    def get_current_parking_count(self, area):
+        count = Parking.objects.filter(
+            parking_area=area,
+            time_end__gte=timezone.now(),
+            time_start__lte=timezone.now(),
+        ).count()
+        return self.blur_count(count)
+
+    def blur_count(self, count):
+        """
+        Returns a blurred count, which is supposed to hide individual
+        parkings.
+        """
+        if count <= 3:
+            return 0
+        else:
+            return count
+
+    class Meta:
+        model = ParkingArea
+        fields = (
+            'id',
+            'current_parking_count',
+        )
+
+
+class PublicAPIParkingAreaStatisticsViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = ParkingArea.objects.all()
+    serializer_class = ParkingAreaStatisticsSerializer
