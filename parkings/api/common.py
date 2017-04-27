c12106ee893b23eec2704f995ced4a3411e448a5
Max <maxgrasbeck@gmail.com>
2017-04-27 14:12:31 +0300

    WGS84 bounding box filter
    The normal bounding box filter won't do, because it isn't aware of the
    reference system being used.

--- parkings/api/common.py
+++ parkings/api/common.py
@@ -2,6 +2,7 @@ import django_filters
 from django.utils import timezone
 from django.utils.translation import ugettext_lazy as _
 from rest_framework import exceptions, serializers
+from rest_framework_gis.filters import InBBoxFilter
 
 from parkings.models import Address, Parking
 
@@ -43,3 +44,15 @@ class ParkingException(exceptions.APIException):
     status_code = 403
     default_detail = _('Unknown error.')
     default_code = 'unknown_error'
+
+
+class WGS84InBBoxFilter(InBBoxFilter):
+    """Works like a normal InBBoxFilter but converts WGS84 to ETRS89"""
+    def get_filter_bbox(self, request):
+        bbox = super().get_filter_bbox(request)
+        if bbox is None:
+            return bbox
+
+        bbox.srid = 4326
+        bbox.transform(3879)
+        return bbox
