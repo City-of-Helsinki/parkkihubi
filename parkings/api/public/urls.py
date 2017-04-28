27bf030df4c2f46eef8cdcd9441bd5d21a22e5cc
Tuomas Haapala <tuomas.haapala@anders.fi>
2017-04-28 14:57:54 +0300

    Fix public API root view links

--- parkings/api/public/urls.py
+++ parkings/api/public/urls.py
@@ -5,8 +5,8 @@ from .parking_area import PublicAPIParkingAreaViewSet
 from .parking_area_statistics import PublicAPIParkingAreaStatisticsViewSet
 
 router = DefaultRouter()
-router.register(r'parking_area', PublicAPIParkingAreaViewSet)
-router.register(r'parking_area_statistics', PublicAPIParkingAreaStatisticsViewSet)
+router.register(r'parking_area', PublicAPIParkingAreaViewSet, base_name='parkingarea')
+router.register(r'parking_area_statistics', PublicAPIParkingAreaStatisticsViewSet, base_name='parkingareastatistics')
 
 urlpatterns = [
     url(r'^', include(router.urls, namespace='v1')),
