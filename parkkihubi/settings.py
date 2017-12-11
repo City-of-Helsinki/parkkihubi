02cc12724f66ba6b466d6f45aea18ade703aecfe
Tuomas Suutari <tuomas.suutari@anders.fi>
2017-12-11 14:48:12 +0200

    settings: DRF: Set DEFAULT_PAGINATION_CLASS
    This is needed with newer Django Rest Framework.

--- parkkihubi/settings.py
+++ parkkihubi/settings.py
@@ -159,6 +159,7 @@ REST_FRAMEWORK = {
     'DEFAULT_FILTER_BACKENDS': ('django_filters.rest_framework.DjangoFilterBackend',),
     'EXCEPTION_HANDLER': 'parkings.exception_handler.parkings_exception_handler',
     'PAGE_SIZE': 20,
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'TEST_REQUEST_DEFAULT_FORMAT': 'json',
 }
 
