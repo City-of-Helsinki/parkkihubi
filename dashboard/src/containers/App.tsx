9b9f56491878e45b6aa4bf3f66f4ea804dd7acba
Tuomas Suutari <tuomas.suutari@anders.fi>
2018-02-19 11:46:29 +0200

    Dashboard: Style login view a bit
    Add a container around the login form so that it will look better.

--- dashboard/src/containers/App.tsx
+++ dashboard/src/containers/App.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import { connect } from 'react-redux';
+import { Container, Row, Col } from 'reactstrap';
 
 import { RootState } from '../types';
 
@@ -16,7 +17,20 @@ class App extends React.Component<Props> {
         if (this.props.isLoading) {
             return null;
         } else if (this.props.showLoginForm) {
-            return (<LoginForm/>);
+            return (
+                <Container fluid={true}>
+                    <Row>
+                        <Col
+                            xs={{size: 10, offset: 1}}
+                            sm={{size: 8, offset: 2}}
+                            md={{size: 6, offset: 3}}
+                            lg={{size: 4, offset: 4}}
+                        >
+                            <LoginForm/>
+                        </Col>
+                    </Row>
+                </Container>
+            );
         } else {
             return (<Dashboard/>);
         }
