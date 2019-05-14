8df59e3c9adf0857a6e51438f04eb708f3043ccc
Deepak Panta <lahiripak@gmail.com>
2019-05-14 15:46:43 +0300

    Permits: Update the data structure of the Permit model
    The requirements for the Permits API were updated on the discussions
    with the API user, so update the data structure of the Permit model to
    match with the new specification.

--- parkings/models/permit.py
+++ parkings/models/permit.py
@@ -1,4 +1,7 @@
-from django.db import models
+from django.contrib.postgres.fields import JSONField
+from django.core.exceptions import ValidationError
+from django.db import models, transaction
+from django.utils.translation import gettext_lazy as _
 
 from .mixins import TimestampedModelMixin
 
@@ -14,79 +17,110 @@ class PermitSeries(TimestampedModelMixin, models.Model):
 
 
 class PermitQuerySet(models.QuerySet):
-    sep = ';'
-
     def active(self):
         return self.filter(series__active=True)
 
     def by_time(self, timestamp):
-        return self.filter(start_time__lte=timestamp, end_time__gte=timestamp)
+        return self.filter(
+            cache_items__in=PermitCacheItem.objects.by_time(timestamp))
 
-    def by_subject(self, subject_identifier):
-        return self.filter(_subjects__contains='{sep}{key}{sep}'.format(
-            sep=self.sep, key=subject_identifier))
+    def by_subject(self, registration_number):
+        return self.filter(
+            cache_items__in=PermitCacheItem.objects.by_subject(registration_number)).distinct()
 
     def by_area(self, area_identifier):
-        return self.filter(_areas__contains='{sep}{key}{sep}'.format(
-            sep=self.sep, key=area_identifier))
+        return self.filter(
+            cache_items__in=PermitCacheItem.objects.by_area(area_identifier))
 
 
 class Permit(TimestampedModelMixin, models.Model):
     series = models.ForeignKey(PermitSeries, on_delete=models.PROTECT)
     external_id = models.CharField(max_length=50, null=True, blank=True)
-    start_time = models.DateTimeField()
-    end_time = models.DateTimeField()
-    _subjects = models.CharField(max_length=1000)
-    _areas = models.CharField(max_length=200)
+    subjects = JSONField()
+    areas = JSONField()
 
     objects = PermitQuerySet.as_manager()
 
     class Meta:
         unique_together = [('series', 'external_id')]
         indexes = [
-            models.Index(fields=['series', 'start_time', '-end_time', 'id']),
+            models.Index(fields=['series', 'id']),
         ]
-        ordering = ('series', 'start_time', '-end_time', 'id')
+        ordering = ('series', 'id')
 
-    def __init__(self, *args, **kwargs):
-        subjects = kwargs.pop('subjects', None)
-        if subjects is not None:
-            kwargs['_subjects'] = _values_to_string(subjects)
-        areas = kwargs.pop('areas', None)
-        if areas is not None:
-            kwargs['_areas'] = _values_to_string(areas)
-        super().__init__(*args, **kwargs)
+    def __str__(self):
+        return ('Permit {}'.format(self.id))
 
-    @property
-    def subjects(self):
-        return _string_to_values(self._subjects)
+    def clean(self):
+        super(Permit, self).clean()
+        for subject in self.subjects:
+            if (
+                'start_time' not in subject
+                or 'end_time' not in subject
+                or 'registration_number' not in subject
+            ):
+                raise ValidationError({'subjects': _('Subjects is not valid')})
+        for area in self.areas:
+            if (
+                'start_time' not in area
+                or 'end_time' not in area
+                or 'area' not in area
+            ):
+                raise ValidationError({'areas': _('Areas is not valid')})
 
-    @subjects.setter
-    def subjects(self, values):
-        self._subjects = _values_to_string(values)
+    def save(self, *args, **kwargs):
+        self.clean()
+        with transaction.atomic():
+            super(Permit, self).save(*args, **kwargs)
+            self._create_cache_items()
 
-    @property
-    def areas(self):
-        return _string_to_values(self._areas)
+    def _create_cache_items(self):
+        """
+        Create the combination of subject and areas for each permit instance
+        for fast lookup. Save it as the instance of PermitCacheItem.
+        """
+        self.cache_items.all().delete()
+        for area in self.areas:
+            for subject in self.subjects:
+                max_start_time = max(subject['start_time'], area['start_time'])
+                min_end_time = min(subject['end_time'], area['end_time'])
 
-    @areas.setter
-    def areas(self, values):
-        self._areas = _values_to_string(values)
+                if max_start_time >= min_end_time:
+                    continue
+                self.cache_items.create(
+                    permit=self,
+                    registration_number=subject['registration_number'],
+                    area_identifier=area['area'],
+                    start_time=max_start_time,
+                    end_time=min_end_time
+                )
+
+
+class PermitCacheItemQuerySet(models.QuerySet):
+    def by_time(self, timestamp):
+        return self.filter(start_time__lte=timestamp, end_time__gte=timestamp)
+
+    def by_subject(self, registration_number):
+        return self.filter(registration_number=registration_number)
+
+    def by_area(self, area_identifier):
+        return self.filter(area_identifier=area_identifier)
+
+
+class PermitCacheItem(models.Model):
+    permit = models.ForeignKey(Permit, related_name="cache_items")
+    registration_number = models.CharField(max_length=30)
+    area_identifier = models.CharField(max_length=10)
+    start_time = models.DateTimeField()
+    end_time = models.DateTimeField()
+
+    objects = PermitCacheItemQuerySet.as_manager()
 
     def __str__(self):
         return (
             '{start_time:%Y-%m-%d %H:%M} -- {end_time:%Y-%m-%d %H:%M} / '
-            '{subjects} / {areas}'
+            '{registration_number} / {area_identifier}'
         ).format(
             start_time=self.start_time, end_time=self.end_time,
-            subjects=' '.join(sorted(self.subjects)),
-            areas=' '.join(sorted(self.areas)))
-
-
-def _values_to_string(values, sep=PermitQuerySet.sep):
-    return '{sep}{vals}{sep}'.format(sep=sep, vals=';'.join(sorted(values)))
-
-
-def _string_to_values(string, sep=PermitQuerySet.sep):
-    stripped = string.strip(sep)
-    return stripped.split(sep) if stripped else []
+            registration_number=self.registration_number,
+            area_identifier=self.area_identifier)
