0f0af19c93a5fbdeb1ce2917fa616ce5eabc4907
Deepak Panta <lahiripak@gmail.com>
2019-05-21 16:48:26 +0300

    Permits: Add PaymentZone model
    Add PaymentZone model to store parking payment zones with their
    geometries.
    
    Refs HP-61

---
+++ parkings/models/constants.py
@@ -0,0 +1,1 @@
+GK25FIN_SRID = 3879  # GK25FIN is a finnish coordiante system that uses meters
---
+++ parkings/models/zone.py
@@ -0,0 +1,18 @@
+from django.contrib.gis.db import models as gis_models
+from django.core.validators import MaxValueValidator, MinValueValidator
+from django.db import models
+from django.utils.translation import ugettext_lazy as _
+
+from .constants import GK25FIN_SRID
+from .mixins import TimestampedModelMixin, UUIDPrimaryKeyMixin
+
+
+class PaymentZone(TimestampedModelMixin, UUIDPrimaryKeyMixin):
+    number = models.IntegerField(verbose_name=_('zone number'), validators=[
+        MinValueValidator(1), MaxValueValidator(3)])
+    name = models.CharField(max_length=40, verbose_name=_('name'))
+    geom = gis_models.MultiPolygonField(
+        srid=GK25FIN_SRID, verbose_name=_('geometry'))
+
+    def __str__(self):
+        return self.name
