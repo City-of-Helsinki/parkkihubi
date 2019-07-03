4145c6feb6bdf2bd15c939d94f1607d923a373b0
Tuomas Suutari <tuomas.suutari@anders.fi>
2019-07-03 18:31:10 +0300

    Configure database sanitizer

---
+++ sanitizers/parkkihubi.py
@@ -0,0 +1,31 @@
+import json
+
+from database_sanitizer.session import hash_text_to_ints
+
+LETTERS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+DIGITS = '0123456789'
+N_LETTERS = len(LETTERS)
+N_DIGITS = len(DIGITS)
+
+
+def sanitize_registration_number(value):
+    if not value or value.strip() in ['', '-']:
+        return value
+
+    normalized = value.replace('-', '').replace(' ', '').upper()
+    ints = hash_text_to_ints(normalized, (16, 16, 16, 16, 16, 16))
+
+    return 'X{letters}{sep}{digits}'.format(
+        letters=''.join(LETTERS[x % N_LETTERS] for x in ints[0:3]),
+        sep=('-' if '-' in value else ''),
+        digits=''.join(DIGITS[x % N_DIGITS] for x in ints[3:6]))
+
+
+def sanitize_permit_subjects(value):
+    if not value or value in ('[]', '{}'):
+        return value
+    subjects = json.loads(value)
+    for subject in subjects:
+        subject['registration_number'] = sanitize_registration_number(
+            subject['registration_number'])
+    return json.dumps(subjects)
