9d23da6d265ac6ca8d4fd3de846e4db23050f5ce
Tuomas Suutari <tuomas.suutari@anders.fi>
2019-07-11 08:26:51 +0300

    sanitizers: Return more empty values as is
    It seems that there are some dummy registration numbers entered from the
    parking terminals such as "-" and "--".  Make all (dummy) registration
    numbers which only contain dashes or spaces to be returned as is.

--- sanitizers/parkkihubi.py
+++ sanitizers/parkkihubi.py
@@ -9,10 +9,16 @@ N_DIGITS = len(DIGITS)
 
 
 def sanitize_registration_number(value):
-    if not value or value.strip() in ['', '-']:
+    if not value:
         return value
 
     normalized = value.replace('-', '').replace(' ', '').upper()
+
+    if not normalized:
+        # Return values containing only spaces or dashes (such as " ", "
+        # - ", "-" and "--") as is
+        return value
+
     ints = hash_text_to_ints(normalized, (16, 16, 16, 16, 16, 16))
 
     return 'X{letters}{sep}{digits}'.format(
