170bedba3ed0c53709b6b56d138b58d0a525220f
Tuomas Suutari <tuomas.suutari@anders.fi>
2019-07-14 13:08:41 +0300

    migrations: Remove interactive arg from loaddata call
    In Django 2.2 the loaddata command does not accept the "interactive"
    argument anymore.

--- parkings/migrations/0013_parking_terminal_data.py
+++ parkings/migrations/0013_parking_terminal_data.py
@@ -6,7 +6,7 @@ from django.db import migrations
 
 def load_parking_terminals_from_fixture(apps, schema_editor):
     call_command('loaddata', 'parking_terminals',
-                 verbosity=0, interactive=False)
+                 verbosity=0)
 
 
 def noop(apps, schema_editor):
