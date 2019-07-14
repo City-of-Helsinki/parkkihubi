eaf42852d7eaf1deddf0e1ca5b47f11606763e47
Tuomas Suutari <tuomas.suutari@anders.fi>
2019-07-14 13:10:49 +0300

    settings: Remove SessionAuthenticationMiddleware
    SessionAuthenticationMiddleware does not exists anymore in Django 2,
    since it has been unconditionally enabled since Django 1.10. See
    https://docs.djangoproject.com/en/2.2/releases/2.0/#miscellaneous

--- parkkihubi/settings.py
+++ parkkihubi/settings.py
@@ -96,7 +96,6 @@ MIDDLEWARE = [
     'django.middleware.csrf.CsrfViewMiddleware',
     'parkkihubi.middleware.AdminTimezoneMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'django.middleware.security.SecurityMiddleware',
