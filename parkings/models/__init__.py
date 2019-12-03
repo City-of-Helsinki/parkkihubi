5971e4479fb996f60f5f1f8433ee1bbd6bee924b
Kristian Klemets <kristian.klemets@anders.fi>
2019-12-03 20:30:56 +0200

    Parking forecast: Add ParkingCount
    Add parking count cache model for regions and parking areas.
    This makes it easier to gather data for the forecast model and also
    it can be used to get precalculated parking count results for the API.

--- parkings/models/__init__.py
+++ parkings/models/__init__.py
@@ -2,6 +2,7 @@ from .operator import Operator
 from .parking import Parking, ParkingQuerySet
 from .parking_area import ParkingArea
 from .parking_check import ParkingCheck
+from .parking_count import ParkingCount
 from .parking_terminal import ParkingTerminal
 from .permit import Permit, PermitArea, PermitLookupItem, PermitSeries
 from .region import Region
@@ -12,6 +13,7 @@ __all__ = [
     'Parking',
     'ParkingArea',
     'ParkingCheck',
+    'ParkingCount',
     'ParkingTerminal',
     'ParkingQuerySet',
     'PaymentZone',
