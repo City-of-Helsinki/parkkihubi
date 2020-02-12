1c8904a42e48027b9b6476e1e7d58b7ef02e77e9
Deepak Panta <deepak.panta@anders.fi>
2020-02-12 13:14:32 +0200

    Operator: Add endpoint to activate permitseries
    Add endpoint to activate permitseries. The id of permit series to
    activate should be in url param. In the post request body, either
    deactivate_others(Boolean) or deactivate_series(list of permit
    series id to deactivate) should be specified. The id specified
    in the url will be activated. The following is case based on
    request body:
    * deactivate_others is true: All other permit series will be
    deactivated.
    * deactivate_series: The permit series specified in
    deactivate_series will be deactivated.

--- parkings/api/operator/permit.py
+++ parkings/api/operator/permit.py
@@ -1,6 +1,10 @@
+from django.db import transaction
+from django.utils.translation import ugettext_lazy as _
 from rest_framework import mixins, serializers
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
-from parkings.models import EnforcementDomain
+from parkings.models import EnforcementDomain, Permit, PermitSeries
 
 from ..common_permit import (
     ActivePermitByExternalIdSerializer, ActivePermitByExternalIdViewSet,
@@ -36,3 +40,53 @@ class OperatorActivePermitByExternalIdViewSet(ActivePermitByExternalIdViewSet):
 
 class OperatorPermitSeriesViewSet(mixins.DestroyModelMixin, PermitSeriesViewSet):
     permission_classes = [OperatorApiHasPermission]
+
+    @action(detail=True, methods=['post'])
+    def activate(self, request, pk=None):
+        with transaction.atomic():
+            obj_to_activate = self.get_object()
+            old_actives = self.queryset.filter(active=True)
+
+            # Always activate the specified permit series regardless of what to deactivate
+            if not obj_to_activate.active:
+                obj_to_activate.active = True
+                obj_to_activate.save()
+
+            deactivate_payload = OperatorPermitSeriesPayload(data=request.data)
+            deactivate_payload.is_valid(raise_exception=True)
+            validated_data = deactivate_payload.validated_data
+
+            if validated_data['deactivate_others']:
+                old_actives.exclude(pk=obj_to_activate.pk).update(active=False)
+            else:
+                ids_to_deactivate = validated_data.get('deactivate_series', [])
+                old_actives.filter(id__in=ids_to_deactivate).exclude(
+                    pk=obj_to_activate.pk
+                ).update(active=False)
+
+            prunable_series = PermitSeries.objects.prunable()
+            Permit.objects.filter(series__in=prunable_series).delete()
+            prunable_series.delete()
+
+            return Response({'status': 'OK'})
+
+
+class OperatorPermitSeriesPayload(serializers.Serializer):
+    deactivate_others = serializers.BooleanField(required=False)
+    deactivate_series = serializers.ListField(
+        child=serializers.IntegerField(), required=False
+    )
+
+    def validate(self, data):
+        if 'deactivate_series' in data:
+            data['deactivate_others'] = False
+
+        if (
+            'deactivate_others' in data
+            or 'deactivate_series' in data
+        ):
+            return super().validate(data)
+
+        raise serializers.ValidationError(
+            _('Either deactivate_others or deactivate_series is required')
+        )
--- parkings/tests/api/operator/test_permit_series.py
+++ parkings/tests/api/operator/test_permit_series.py
@@ -1,6 +1,8 @@
+import pytest
 from django.urls import reverse
 from rest_framework.status import (
-    HTTP_200_OK, HTTP_201_CREATED, HTTP_204_NO_CONTENT, HTTP_404_NOT_FOUND)
+    HTTP_200_OK, HTTP_201_CREATED, HTTP_204_NO_CONTENT, HTTP_400_BAD_REQUEST,
+    HTTP_404_NOT_FOUND)
 
 from parkings.models import PermitSeries
 
@@ -46,3 +48,85 @@ def test_operator_can_delete_permitseries_owned_by_herself(
     assert response.status_code == HTTP_204_NO_CONTENT
     assert PermitSeries.objects.count() == 1
     assert PermitSeries.objects.first().id != permit_series.pk
+
+
+def get_activate_url(obj):
+    return reverse('operator:v1:permitseries-detail', kwargs={'pk': obj.pk})
+
+
+def _create_permit_series(count=3, owner=None, active=False):
+    for _ in range(count):
+        PermitSeries.objects.create(active=active, owner=owner)
+
+
+@pytest.mark.parametrize('deactivate_others', [True, False])
+def test_operator_can_deactivate_all_other_series_activating_main(
+    operator_api_client, operator, deactivate_others
+):
+    _create_permit_series(count=5, owner=operator.user, active=True)
+    assert PermitSeries.objects.count() == 5
+
+    series_to_activate = PermitSeries.objects.first()
+    series_to_activate.active = False
+    series_to_activate.save()
+
+    response = operator_api_client.post(
+        '{}activate/'.format(get_activate_url(series_to_activate)),
+        data={'deactivate_others': deactivate_others},
+    )
+
+    assert response.status_code == HTTP_200_OK
+    series_to_activate.refresh_from_db()
+    assert series_to_activate.active
+    assert response.json()['status'] == 'OK'
+
+    if deactivate_others:
+        assert PermitSeries.objects.filter(active=True).count() == 1
+    else:
+        assert PermitSeries.objects.filter(active=True).count() == 5
+
+
+def test_operator_can_deactivate_specified_series_activating_main(
+    operator_api_client, operator
+):
+    _create_permit_series(count=5, owner=operator.user, active=True)
+    assert PermitSeries.objects.count() == 5
+
+    series_to_activate = PermitSeries.objects.first()
+    series_to_activate.active = False
+    series_to_activate.save()
+
+    series_to_deactivate = list(
+        PermitSeries.objects.values_list('id', flat=True)[3:]
+    )
+
+    response = operator_api_client.post(
+        '{}activate/'.format(get_activate_url(series_to_activate)),
+        data={'deactivate_series': series_to_deactivate},
+    )
+
+    assert response.status_code == HTTP_200_OK
+    series_to_activate.refresh_from_db()
+    assert series_to_activate.active
+    assert response.json()['status'] == 'OK'
+    assert PermitSeries.objects.filter(active=False).count() == 2
+    assert PermitSeries.objects.filter(active=True).count() == 3
+
+    for series in PermitSeries.objects.filter(active=False):
+        assert series.id in series_to_deactivate
+
+
+def test_operator_activate_series_fails_when_required_post_data_is_missing(
+    operator_api_client, operator
+):
+    _create_permit_series(count=1, owner=operator.user, active=False)
+    series_to_activate = PermitSeries.objects.first()
+
+    response = operator_api_client.post(
+        '{}activate/'.format(get_activate_url(series_to_activate)),
+    )
+
+    assert response.status_code == HTTP_400_BAD_REQUEST
+    assert response.json()['non_field_errors'] == [
+        'Either deactivate_others or deactivate_series is required'
+    ]
