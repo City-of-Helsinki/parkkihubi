ea62e18b8247ca12d6b0c36872cad148765d70cd
Tuomas Suutari <tuomas.suutari@anders.fi>
2020-02-19 12:35:59 +0200

    Add merge migration 0032
    Merge the new forecast parking_count migration with the migrations from
    the multicity branch.

---
+++ parkings/migrations/0032_merge_parking_count.py
@@ -0,0 +1,9 @@
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+    dependencies = [
+        ('parkings', '0031_default_domain'),
+        ('parkings', '0029_parkingcount'),
+    ]
+    operations = []
