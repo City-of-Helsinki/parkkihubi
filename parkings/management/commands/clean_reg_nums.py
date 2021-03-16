06fb6b31ecb5e79cc08a2c494208dbe406bd6d58
Aman Yadav <amanpdyadav@gmail.com>
2021-03-16 21:22:20 +0200

    Add management commands for the anonymization

---
+++ parkings/management/commands/clean_reg_nums.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+"""
+Anonymize the registration number from Parking, ParkingCheck and Permit
+that are expired 24 hours ago.
+"""
+from django.core.management.base import BaseCommand
+
+from parkings.anonymization import (
+    anonymize_parking_check_registration_numbers,
+    anonymize_parking_registration_numbers,
+    anonymize_permit_registration_numbers)
+
+
+class Command(BaseCommand):
+    help = __doc__.strip().splitlines()[0]
+
+    def handle(self, *args, **options):
+        anonymize_parking_registration_numbers()
+        anonymize_parking_check_registration_numbers()
+        anonymize_permit_registration_numbers()
