f69fa6cb2de768f885d568b90ce723159afb9c38
Tuomas Suutari <tuomas.suutari@anders.fi>
2023-10-20 16:45:11 +0300

    MethodOverrideMiddleware
    Implement the method override feature as was documented in the previous
    commit:
    
    Make it possible to override the HTTP method of the API calls via a
    query parameter named "method".  This allows using the API with some
    limited clients which don't support all HTTP methods.
    
    To enable this feature the real HTTP method has to be POST and (for now)
    only supported methods are DELETE, PATCH and PUT.

--- parkkihubi/middleware.py
+++ parkkihubi/middleware.py
@@ -2,6 +2,7 @@ import contextlib
 from functools import lru_cache
 
 from django.conf import settings
+from django.http import HttpResponseNotAllowed
 from django.urls import reverse
 from django.utils import timezone
 
@@ -29,3 +30,37 @@ class AdminTimezoneMiddleware:
 @lru_cache()
 def get_admin_url_path_prefix():
     return reverse('admin:index')
+
+
+class MethodOverrideMiddleware:
+    """
+    Override the HTTP method with a query parameter.
+
+    Allows overriding the HTTP method using the `method` query string
+    parameter.  Only works with POST requests and allows overriding the
+    method to DELETE, PATCH or PUT.
+    """
+
+    allowed_override_methods = {"DELETE", "PATCH", "PUT"}
+
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        if request.method == "POST" and "method" in request.GET:
+            method = request.GET["method"]
+
+            if method not in self.allowed_override_methods:
+                return HttpResponseNotAllowed(
+                    permitted_methods=self.allowed_override_methods,
+                    reason="Method Override Not Allowed",
+                )
+
+            # Use the specified method instead of POST
+            request.method = method
+
+            # Remove the method parameter from the query string
+            request.GET = request.GET.copy()
+            del request.GET["method"]
+
+        return self.get_response(request)
