32b9faff79df5bcec4d330816cd2c9111cf174a5
Tuomas Suutari <tuomas.suutari@anders.fi>
2023-11-24 07:33:28 +0200

    Management commands: Make "allowed_user" optional
    Make the "allowed_user" parameter optional, since now it's possible to
    create the PermitAreas without any allowed users and it could make sense
    to import new areas without assigning them to any users at import time.
    
    Refs HPH-36

--- parkings/importers/geojson_permit_areas.py
+++ parkings/importers/geojson_permit_areas.py
@@ -16,15 +16,18 @@ class PermitAreaImporter(GeoJsonImporter):
     Imports permit area data
     """
 
-    def import_permit_areas(self, geojson_file_path, allowed_user):
+    def import_permit_areas(self, geojson_file_path, allowed_user=None):
         permit_area_dicts = self.read_and_parse(geojson_file_path)
         count = self._save_permit_areas(permit_area_dicts, allowed_user)
         logger.info('Created or updated {} permit areas'.format(count))
 
     @transaction.atomic
-    def _save_permit_areas(self, permit_areas_dict, allowed_user):
+    def _save_permit_areas(self, permit_areas_dict, allowed_user=None):
         logger.info('Saving permit areas.')
-        user = get_user_model().objects.filter(username=allowed_user).get()
+        if allowed_user is not None:
+            user = get_user_model().objects.filter(username=allowed_user).get()
+        else:
+            user = None
         default_domain = self.get_default_domain()
         count = 0
         permit_area_ids = []
@@ -34,7 +37,8 @@ class PermitAreaImporter(GeoJsonImporter):
                 identifier=area_dict['identifier'],
                 domain=domain,
                 defaults=area_dict)
-            permit_area.allowed_users.add(user)
+            if user is not None:
+                permit_area.allowed_users.add(user)
             permit_area_ids.append(permit_area.pk)
             count += 1
         return count
--- parkkihubi_hel/management/commands/import_permit_areas.py
+++ parkkihubi_hel/management/commands/import_permit_areas.py
@@ -7,7 +7,7 @@ class Command(BaseCommand):
     help = 'Uses the PermitAreaImporter to create permit areas'
 
     def add_arguments(self, parser):
-        parser.add_argument('allowed_user')
+        parser.add_argument('allowed_user', nargs='?', type=str, default=None)
 
     def handle(self, *args, allowed_user=None, **options):
         PermitAreaImporter().import_permit_areas(allowed_user)
