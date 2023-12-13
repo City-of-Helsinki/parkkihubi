a62bb00621e6809a88e04bd02994086c990b8c83
Terhi Salonen <terhi.salonen@haltu.fi>
2023-12-13 16:19:42 +0200

    Update 'url' to 're_path' for parkings/api/auth.

--- parkings/api/auth/urls.py
+++ parkings/api/auth/urls.py
@@ -1,8 +1,8 @@
-import drf_jwt_2fa.urls
+from parkings.api.auth import drf_jwt_2fa_urls
 
 from ..url_utils import versioned_url
 
 app_name = 'auth'
 urlpatterns = [
-    versioned_url('v1', drf_jwt_2fa.urls),
+    versioned_url('v1', drf_jwt_2fa_urls),
 ]
